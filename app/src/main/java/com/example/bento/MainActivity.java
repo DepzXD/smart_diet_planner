244c8d076fad5da1c01717ae4d77dc1b7dbd4a18
depzXD <dk721572@gmail.com>
2022-04-13 21:52:42 +0530

    Added Bottom Navigation Components

--- app/src/main/java/com/example/bento/MainActivity.java
+++ app/src/main/java/com/example/bento/MainActivity.java
@@ -4,11 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+  private BottomNavigationView bottomNavigationView;
+  private final HomeFragment homeFragment = new HomeFragment();
+  private final ProfileFragment profileFragment = new ProfileFragment();
+  private final InterviewFragment interviewFragment = new InterviewFragment();
+  private final ProgressFragment progressFragment = new ProgressFragment();
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+    bottomNavigationView.setOnItemSelectedListener(item -> {
+      if (item.getItemId() == R.id.home) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+        return true;
+      } else if(item.getItemId() == R.id.profile) {
+          getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+          return true;
+      } else if (item.getItemId() == R.id.progress) {
+          getSupportFragmentManager().beginTransaction().replace(R.id.container, progressFragment).commit();
+          return true;
+      } else if (item.getItemId() == R.id.interview) {
+          getSupportFragmentManager().beginTransaction().replace(R.id.container, interviewFragment).commit();
+          return true;
+      } else { return false; }
+
+    });
   }
 }
