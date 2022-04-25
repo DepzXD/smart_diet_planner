a890b7a4890d387ebf6710b30dfc88363641c834
kushagrpaliwal <kushagrpaliwal2@gmail.com>
2022-04-25 09:21:23 +0530

    created validation (Signin,Signup,personalinfo) section .

---
+++ app/src/main/java/com/example/bento/Signin.java
@@ -0,0 +1,38 @@
+package com.example.bento;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openactivity2();
+            }
+        });
+
+
+
+    }
+
+    public void openactivity2(){
+
+        Intent intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+
+
+
+
+}
